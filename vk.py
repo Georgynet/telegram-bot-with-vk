e878903c894b9fa48e334c709934ea3fa115b0b1
Georg Spack <georgynet@gmail.com>
2021-10-04 12:44:55 +0200

    change method get posts

--- vk.py
+++ vk.py
@@ -1,9 +1,8 @@
-import vk_api
 import config
 
 
-def get_posts(vk_session, max_posts):
-    tools = vk_api.VkTools(vk_session)
-    wall = tools.get_all('wall.get', max_posts, values={'owner_id': config.wall_id})
+def get_posts(vk_session, max_posts) -> list:
+    vk = vk_session.get_api()
+    wall = vk.wall.get(count=max_posts, owner_id=config.wall_id)
 
     return wall['items']
